3dfc3bca549dffee2994a5a5488d0e66f19567b1
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-10-02 17:21:59 +0100

    Video 6 Macoratti: Layouts - StackLayout

--- MauiDemo/App.xaml.cs
+++ MauiDemo/App.xaml.cs
@@ -11,7 +11,7 @@
             navPage.BarBackground = Colors.Yellow;
             navPage.BarTextColor = Colors.Blue;
 
-            MainPage = new TabbedPageDemo();
+            MainPage = new StackLayoutDemo();
         }
     }
 }
