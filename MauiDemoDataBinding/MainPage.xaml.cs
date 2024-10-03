2633b0b93e9dece4b779b3fab73881064e59b094
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-10-03 18:58:05 +0100

    Video 15 Macoratti: Data Binding I

---
+++ MauiDemoDataBinding/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MauiDemoDataBinding.Models;
+
+namespace MauiDemoDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //private void OnCounterClicked(object sender, EventArgs e)
+        //{
+        //    var produto = new Produto
+        //    {
+        //        Nome = "iPhone 5",
+        //        Preco = 5000.00m,
+        //        Stock = 5
+        //    };
+
+        //    //Binding produtoBinding = new Binding();
+        //    //produtoBinding.Source = produto;
+        //    //produtoBinding.Path = "Nome";
+
+        //    // lblNome.SetBinding(Label.TextProperty, produtoBinding);
+
+        //    Binding nomeBinding = new Binding
+        //    {
+        //        Source = produto,
+        //        Path = "Nome",
+        //        StringFormat = "Produto: {0}"
+        //    };
+
+        //    lblNome.SetBinding(Label.TextProperty, nomeBinding);
+
+        //    Binding precoBinding = new Binding
+        //    {
+        //        Source = produto,
+        //        Path = "Preco",
+        //        StringFormat = "Preço: {0}"
+        //    };
+
+        //    lblPreco.SetBinding(Label.TextProperty, precoBinding);
+
+        //    Binding stockBinding = new Binding
+        //    {
+        //        Source = produto,
+        //        Path = "Stock",
+        //        StringFormat = "Stock: {0}"
+        //    };
+
+        //    lblStock.SetBinding(Label.TextProperty, stockBinding);
+        //}
+    }
+
+}
