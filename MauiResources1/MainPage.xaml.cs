4e3f4c073b35d556c736bff0c981941da5070d1d
silviapoliveira <correiodasilviaoliveira@gmail.com>
2024-10-04 10:53:58 +0100

    Video 18 Macoratti: Images, Fonts, Icons and Other Arquives

---
+++ MauiResources1/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+
+namespace MauiResources1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await LoadMauiAsset();
+        }
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("demo.json");
+            using var reader = new StreamReader(stream);
+
+            var contents = reader.ReadToEnd();
+
+            var pessoa = JsonSerializer.Deserialize<Pessoa>(contents);
+
+            BindingContext = pessoa;
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public string Sobrenome { get; set; }
+
+            public int Idade { get; set; }
+
+            public string Foto { get; set; }
+        }
+    }
+
+}
---
+++ MauiResources1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiResources1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Pacifico.ttf", "MinhaFonte");
+                    fonts.AddFont("fontello.ttf", "IconsFont");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
